9cd3e1d84adda6b9b2861dc269ae8394a3e9060a
Jhampier Tucno <yantucnoperez@gmail.com>
2023-07-04 18:15:10 -0500

    header terminado

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'shared-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
+  public click: boolean = false;
+
+  public nav = [
+    { name: 'Inicio', route: 'product' },
+    { name: 'Nosotros', route: 'product/nosotros' },
+    { name: 'Productos', route: 'product/productos' },
+    { name: 'Tips', route: 'product/tips' },
+    { name: 'Contacto', route: 'product/contacto' }
+  ]
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 950) {
+      this.click = false;
+    }
+  }
+
+  onClick() {
+    this.click = !this.click;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderContactoComponent } from './components/header-contacto/header-contacto.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +15,9 @@ import { HeaderContactoComponent } from './components/header-contacto/header-con
     HeaderContactoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   exports: [
     HeaderComponent,
