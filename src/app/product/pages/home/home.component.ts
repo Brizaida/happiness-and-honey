428bf7d312cb0b91eb4adc7df515abb3447e5bf9
Jhampier Tucno <yantucnoperez@gmail.com>
2023-07-10 12:55:36 -0500

    carrousel agregado - video agregado

--- src/app/product/pages/home/home.component.ts
+++ src/app/product/pages/home/home.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public carrousel = [
+    {
+      title: 'First slide label',
+      description: 'Desafía tus sentidos y déjate seducir por la tentación más natural',
+      image: '../../../../assets/fondo.jpg',
+    },
+    {
+      title: 'First slide label',
+      description: 'Nuestra miel es el elixir que despierta tus gustos más refinados y te sumerge en un universo de sabores irresistibles',
+      image: '../../../../assets/fondo.jpg',
+    },
+    {
+      title: 'First slide label',
+      description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+      image: '../../../../assets/fondo.jpg',
+    },
+  ]
 }
